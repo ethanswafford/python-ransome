145d3d9817f644486a5aae13cda1d29806fefe34
Ethan Swafford <ethanswafford@yahoo.com>
2022-05-23 13:47:38 -0400

    secPush

--- decrypt.py
+++ decrypt.py
@@ -16,9 +16,16 @@ print(files)
 with open("thekey.key", "rb") as key:
         secretkey = key.read()
 
-for file in files:
-        with open(file, "rb") as thefile:
+secretphrase = "rescue"
+user_phrase = input("enter decrypt password\n")
+
+if user_phrase == secretphrase:
+        for file in files:
+            with open(file, "rb") as thefile:
                 contents = thefile.read
-        contents_decrypted = Fernet(secretkey).decrypt(contents)
-        with open(file, "wb") as thefile:
+            contents_decrypted = Fernet(secretkey).decrypt(contents)
+            with open(file, "wb") as thefile:
                 thefile.write(contents_decrypted)  
+            print("file decrypt successful\n")
+else:
+        print("invalid password, attempt has been logged\n")    
